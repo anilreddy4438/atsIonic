9e4fc0a73da2f9f85544b3a0bf4bdf14a239e216
conifer <anil@coniferlabs.in>
2018-06-15 11:08:58 +0530

    csv

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ChangeDetectorRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Tab1Model} from "../../components/tab1/tab1.model";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
@@ -12,7 +12,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class HomePage {
   tab1Form: FormGroup;
     tab1: Tab1Model;
-  constructor(public navCtrl: NavController,private formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController,private formBuilder: FormBuilder,private cd: ChangeDetectorRef) {
     this.tab1 = new Tab1Model()
     console.log(this.tab1 )
     this.tab1Form = this.formBuilder.group({
@@ -45,4 +45,49 @@ export class HomePage {
 
   }
 
+
+  onFileChange(event) {
+    const reader = new FileReader();
+    console.log(event);
+    if(event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.tab1Form.patchValue({
+          file: reader.result
+       });
+
+        // need to run CD since file load runs outside of zone
+        this.cd.markForCheck();
+      };
+    }
+  }
+
+
+
+public changeListener(files: FileList){
+  console.log(files);
+  if(files && files.length > 0) {
+     let file : File = files.item(0);
+       let reader: FileReader = new FileReader();
+       reader.readAsText(file);
+       reader.onload = (e) => {
+          let csv: string = reader.result;
+          let headers = csv.split("\n");
+         let headersArr = headers[0].split(",");
+         let dataArr = headers.slice(1,-1);
+         let arr =   dataArr.map(data =>{
+           return data.split(",")
+         });
+      let objects  =  arr.map((data) =>{
+           let obj ={};
+           headersArr.map((head,index) =>{
+             obj[head] = data[index]
+           })
+        return obj
+         })
+       }
+    }
+}
 }
